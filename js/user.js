ecce601d17e7a85713100f0862392f96ce34e4c9
shinyelee <shinye0213@naver.com>
2021-02-02 23:59:50 +0900

    제코베 [Summary 시리즈] 30분 JS 요약 2부 (13)

---
+++ js/user.js
@@ -0,0 +1,132 @@
+var user = [
+	  {
+		    "_id": "60196646767256e4e4e2e00e",
+		    "age": 32,
+		    "eyeColor": "green",
+		    "name": "Annie Dodson",
+		    "gender": "female",
+		    "company": "MIRACLIS",
+		    "email": "anniedodson@miraclis.com",
+		    "phone": "+1 (950) 538-3095"
+		  },
+		  {
+		    "_id": "601966463cbdc83c5d0c287e",
+		    "age": 22,
+		    "eyeColor": "green",
+		    "name": "Reilly Strong",
+		    "gender": "male",
+		    "company": "PROXSOFT",
+		    "email": "reillystrong@proxsoft.com",
+		    "phone": "+1 (827) 492-3038"
+		  },
+		  {
+		    "_id": "601966465b36c4962b241d31",
+		    "age": 24,
+		    "eyeColor": "blue",
+		    "name": "Roth Daniels",
+		    "gender": "male",
+		    "company": "TERRAGO",
+		    "email": "rothdaniels@terrago.com",
+		    "phone": "+1 (834) 538-3456"
+		  },
+		  {
+		    "_id": "60196646b66ce2dbbf64cb61",
+		    "age": 29,
+		    "eyeColor": "green",
+		    "name": "Jessica Rojas",
+		    "gender": "female",
+		    "company": "DIGIQUE",
+		    "email": "jessicarojas@digique.com",
+		    "phone": "+1 (811) 402-2443"
+		  },
+		  {
+		    "_id": "601966468e88307cbcc306a2",
+		    "age": 25,
+		    "eyeColor": "green",
+		    "name": "Roslyn Stanley",
+		    "gender": "female",
+		    "company": "PURIA",
+		    "email": "roslynstanley@puria.com",
+		    "phone": "+1 (805) 527-2068"
+		  },
+		  {
+		    "_id": "60196646a3780b922436b734",
+		    "age": 26,
+		    "eyeColor": "brown",
+		    "name": "Craft Salazar",
+		    "gender": "male",
+		    "company": "ZAJ",
+		    "email": "craftsalazar@zaj.com",
+		    "phone": "+1 (952) 567-3759"
+		  },
+		  {
+		    "_id": "60196646b922f79fa4d364e1",
+		    "age": 21,
+		    "eyeColor": "green",
+		    "name": "Latasha Dickerson",
+		    "gender": "female",
+		    "company": "VERTON",
+		    "email": "latashadickerson@verton.com",
+		    "phone": "+1 (822) 595-3277"
+		  },
+		  {
+		    "_id": "601966461cf03d8a789d0cb0",
+		    "age": 40,
+		    "eyeColor": "green",
+		    "name": "Vazquez Sloan",
+		    "gender": "male",
+		    "company": "TERASCAPE",
+		    "email": "vazquezsloan@terascape.com",
+		    "phone": "+1 (955) 499-2940"
+		  },
+		  {
+		    "_id": "601966468766487af34f061b",
+		    "age": 39,
+		    "eyeColor": "blue",
+		    "name": "Hinton Mathews",
+		    "gender": "male",
+		    "company": "ENQUILITY",
+		    "email": "hintonmathews@enquility.com",
+		    "phone": "+1 (909) 562-2866"
+		  },
+		  {
+		    "_id": "6019664622536c78d2e28f61",
+		    "age": 26,
+		    "eyeColor": "brown",
+		    "name": "Anna Thomas",
+		    "gender": "female",
+		    "company": "OCTOCORE",
+		    "email": "annathomas@octocore.com",
+		    "phone": "+1 (938) 500-2437"
+		  },
+		  {
+		    "_id": "601966468c821e4c5847a226",
+		    "age": 38,
+		    "eyeColor": "blue",
+		    "name": "Henderson Lott",
+		    "gender": "male",
+		    "company": "BILLMED",
+		    "email": "hendersonlott@billmed.com",
+		    "phone": "+1 (886) 509-2251"
+		  },
+		  {
+		    "_id": "60196646ff9a6f4a63f2a547",
+		    "age": 40,
+		    "eyeColor": "blue",
+		    "name": "Kris Vincent",
+		    "gender": "female",
+		    "company": "FLYBOYZ",
+		    "email": "krisvincent@flyboyz.com",
+		    "phone": "+1 (841) 492-3407"
+		  },
+		  {
+		    "_id": "6019664679b5e561c5935daa",
+		    "age": 27,
+		    "eyeColor": "blue",
+		    "name": "Horn House",
+		    "gender": "male",
+		    "company": "PROVIDCO",
+		    "email": "hornhouse@providco.com",
+		    "phone": "+1 (838) 420-2198"
+		  }
+		]
